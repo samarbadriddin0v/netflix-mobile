15ea7fb9b508a2fd93a2a435aab4db7302220992
samarbadriddin0v <info@sammi.ac>
2023-11-12 21:06:45 +0200

    Add api key

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
-export const API_KEY = "7db5efb642c5d47759bd2c2b413fbea8";
+export const API_KEY = "your api key";
 export const BASE_URL = "https://api.themoviedb.org/3";
 export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p";
 
